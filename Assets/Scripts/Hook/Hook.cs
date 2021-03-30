bbb946535f8afdb887177da50a245d956f2708a7
Emre Gül <48657990+emregull@users.noreply.github.com>
2021-03-31 01:09:00 +0300

    FishMaster-Clone

---
+++ Assets/Scripts/Hook/Hook.cs
@@ -0,0 +1,113 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using DG.Tweening;
+public class Hook : MonoBehaviour
+{
+    public Transform hookedTransform;
+
+    private Camera mainCamera;
+    private Collider2D coll;
+
+    private int lenght;
+    private int strength;
+    private int fishCount;
+
+    private bool canMove = true;
+    private Tweener cameraTween;
+    private List<Fish> hookedFishes;
+    void Awake()
+    {
+        mainCamera = Camera.main;
+        coll = GetComponent<Collider2D>();
+        hookedFishes = new List<Fish>();
+    }
+
+
+    void Update()
+    {
+        if (canMove && Input.GetMouseButton(0))
+        {
+            Vector3 vector = mainCamera.ScreenToWorldPoint(Input.mousePosition);
+            Vector3 position = transform.position;
+            position.x = vector.x;
+            transform.position = position;
+        }
+    }
+    public void StartFishing()
+    {
+        lenght = IdleManager.instance.length - 20;
+        strength = IdleManager.instance.strength;
+        fishCount = 0;
+        float time = (-lenght) * 0.1f;
+
+        cameraTween = mainCamera.transform.DOMoveY(lenght, 1 + time * 0.25f, false).OnUpdate(delegate
+        {
+            if (mainCamera.transform.position.y <= -11)
+                transform.SetParent(mainCamera.transform);
+        }).OnComplete(delegate
+        {
+            coll.enabled = true;
+            cameraTween = mainCamera.transform.DOMoveY(0, time * 5f, false).OnUpdate(delegate
+            {
+                if (mainCamera.transform.position.y >= -25f)
+                    StopFishing();
+            });
+        });
+        ScreenManager.instance.CheckScreen(Screens.GAME);
+        coll.enabled = false;
+        canMove = true;
+        hookedFishes.Clear();
+    }
+    public void StopFishing()
+    {
+        canMove = false;
+        cameraTween.Kill(false);
+        cameraTween = mainCamera.transform.DOMoveY(0, 2, false).OnUpdate(delegate
+        {
+            if (mainCamera.transform.position.y >= -11)
+            {
+                transform.SetParent(null);
+                transform.position = new Vector2(transform.position.x, -6);
+            }
+        }).OnComplete(delegate
+        {
+            transform.position = Vector2.down * 6;
+            coll.enabled = true;
+            int num = 0;
+            for (int i = 0; i < hookedFishes.Count; i++)
+            {
+                hookedFishes[i].transform.SetParent(null);
+                hookedFishes[i].ResetFish();
+                num += hookedFishes[i].Type.price;
+            }
+            IdleManager.instance.totalGain = num;
+            ScreenManager.instance.CheckScreen(Screens.END);
+        });
+    }
+
+    private void OnTriggerEnter2D(Collider2D target)
+    {
+        if (target.CompareTag("Fish") && fishCount != strength)
+        {
+            fishCount++;
+            Fish component = target.GetComponent<Fish>();
+            component.Hooked();
+            hookedFishes.Add(component);
+            target.transform.SetParent(transform);
+            target.transform.position = hookedTransform.position;
+            target.transform.rotation = hookedTransform.rotation;
+            target.transform.localScale = Vector3.one;
+
+            target.transform.DOShakeRotation(5, Vector3.forward * 45, 10, 90, false).SetLoops(1, LoopType.Yoyo).OnComplete(delegate
+            {
+                target.transform.rotation = Quaternion.identity;
+            });
+
+            if (fishCount==strength)
+            {
+                StopFishing();
+            }
+        }
+    }
+}
---
+++ Assets/Scripts/Manager Screen/IdleManager.cs
@@ -0,0 +1,147 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using System;
+public class IdleManager : MonoBehaviour
+{
+    [HideInInspector]
+    public int length;
+
+    [HideInInspector]
+    public int strength;
+
+    [HideInInspector]
+    public int offlineEarnings;
+
+    [HideInInspector]
+    public int lengthCost;
+
+    [HideInInspector]
+    public int strengthCost;
+
+    [HideInInspector]
+    public int offlineEarningsCost;
+
+    [HideInInspector]
+    public int wallet;
+
+    [HideInInspector]
+    public int totalGain;
+
+    private int[] costs = new int[]
+    {
+        100,
+        200,
+        400,
+        600,
+        800,
+        1000,
+        1300,
+        1500,
+        1900,
+        2200,
+        2800,
+        3200,
+        4200,
+        5400,
+        6700,
+        7500,
+        7501,
+        7502,
+        7503,
+        7504,
+        7505,
+        7506,
+        7507,
+        7508
+
+    };
+
+    public static IdleManager instance;
+    void Awake()
+    {
+        if (IdleManager.instance)
+        {
+            UnityEngine.Object.Destroy(gameObject);
+        }
+        else
+        {
+            IdleManager.instance = this;
+        }
+
+        length = -PlayerPrefs.GetInt("Length", 30);
+        strength = PlayerPrefs.GetInt("Strength", 3);
+        offlineEarnings = PlayerPrefs.GetInt("Offline", 3);
+        lengthCost = costs[-length / 100 - 3];
+        strengthCost = costs[strength-3];
+        offlineEarningsCost = costs[offlineEarnings-3];
+        wallet = PlayerPrefs.GetInt("Wallet", 0);
+    }
+
+    private void OnApplicationPause(bool paused)
+    {
+        if (paused)
+        {
+            DateTime now = DateTime.Now;
+            PlayerPrefs.SetString("Date", now.ToString());
+            MonoBehaviour.print(now.ToString());
+        }
+        else
+        {
+            string @string = PlayerPrefs.GetString("Date", string.Empty);
+            if (@string != string.Empty)
+            {
+                DateTime date = DateTime.Parse(@string);
+                totalGain = (int)((DateTime.Now - date).TotalMinutes * offlineEarnings + 1.0);
+                ScreenManager.instance.CheckScreen(Screens.RETURN);
+            }
+        }
+    }
+
+    private void OnApplicationQuit()
+    {
+        OnApplicationPause(true);
+    }
+
+    public void BuyLength()
+    {
+        length -= 10;
+        wallet -= lengthCost;
+        lengthCost = costs[strength - 3];
+        PlayerPrefs.SetInt("Length", -length);
+        PlayerPrefs.SetInt("Wallet", wallet);
+        ScreenManager.instance.CheckScreen(Screens.MAIN);
+    }
+    public void BuyStrength()
+    {
+        strength++;
+        wallet -= strengthCost;
+        strengthCost = costs[strength - 3];
+        PlayerPrefs.SetInt("Strength", strength);
+        PlayerPrefs.SetInt("Wallet", wallet);
+        ScreenManager.instance.CheckScreen(Screens.MAIN);
+    }
+    public void BuyOfflineEarnings()
+    {
+        offlineEarnings++;
+        wallet -= offlineEarningsCost;
+        offlineEarningsCost = costs[offlineEarnings - 3];
+        PlayerPrefs.SetInt("Offline", offlineEarnings);
+        PlayerPrefs.SetInt("Wallet", wallet);
+        ScreenManager.instance.CheckScreen(Screens.MAIN);
+    }
+
+    public void CollectMoney()
+    {
+        wallet += totalGain;
+        PlayerPrefs.SetInt("Wallet", wallet);
+        ScreenManager.instance.CheckScreen(Screens.MAIN);
+    }
+
+    public void CollectMoney2x()
+    {
+        wallet += totalGain*2;
+        PlayerPrefs.SetInt("Wallet", wallet);
+        ScreenManager.instance.CheckScreen(Screens.MAIN);
+    }
+}
